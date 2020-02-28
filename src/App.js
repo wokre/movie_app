bd2a4441483b1ce421fb5912c12a417a94441ec6
wokre <worke95@gmail.com>
2020-02-29 01:41:58 +0900

    component with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Potato from './Potato';
 
+//props, props.object
+//ex) props ==> 태그의 전체 속성값 object형식으로 전달
+//ex) props.favorite ==> 속성 중 하나의 값 확인 = { favorite }
+
+function Food ({favorite}) {
+  // return <h1>I like {props.favorite}</h1>
+  return <h1>I like {favorite}</h1>
+}
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Potato />
-    </div>
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samgiopsal" />
+      <Food favorite="chukumi" />
+      <Food favorite="bulgogi" />
+      </div>
   );
 }
 
