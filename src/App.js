971935c1ec3bb8b70ee8e525d82528f43c1ded3e
wokre <worke95@gmail.com>
2020-04-21 23:53:19 +0900

    0421 call movie api

--- src/App.js
+++ src/App.js
@@ -1,70 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-//create function
-const iLikeFood = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating: 4.9
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating: 4.8
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating: 4.7
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating: 4.6
+class App extends React.Component {
+
+  state = {
+    isLoading: true
+  } 
+
+  //비동기 함수 처리 async, await사용
+  getMovies = async() => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   }
-];
 
-//props, props.object
-//ex) props ==> 태그의 전체 속성값 object형식으로 전달
-//ex) props.favorite ==> 속성 중 하나의 값 확인 = { favorite }
+  componentDidMount() {
+    this.getMovies();
+  }
 
-function Food ({name, pictures, rating}) {
-  // return <h1>I like {props.favorite}</h1>
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={pictures} alt={name}/>
-  </div>
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pictures: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!!!</h1>
-      {iLikeFood.map(dish => <Food key={dish.id} name={dish.name} pictures={dish.image} rating={dish.rating}/>)}
-    </div>
-  );
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
