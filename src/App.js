346ae7626a3c2540a2917c018ff8d896330d065b
wokre <worke95@gmail.com>
2020-04-22 23:08:42 +0900

    0422 create css file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import Movie from './Movie';
+import "./App.css";
 
 class App extends React.Component {
 
   state = {
-    isLoading: true
-  } 
+    isLoading: true,
+    movies: []
+  }
 
   //비동기 함수 처리 async, await사용
   getMovies = async() => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data: { data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   }
 
   componentDidMount() {
@@ -18,8 +22,31 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies} = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {
+              movies.map(movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.title}
+                  year={movie.year}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))
+            }
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
